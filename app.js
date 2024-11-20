b77ea1047e234d8e9ac5c59f5a0a6bedc573d6a6
Haythem <sinnner6@gmail.com>
2024-11-20 10:25:19 +0100

    created server using node

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("API")
+import { createServer } from 'node:http'
+
+createServer(async (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Allow-Control-Allow-Origin', '*')
+    res.setHeader('Allow-Control-Allow-Headers', 'Content-Type, Accept, Origin, Authorization')
+    res.setHeader('Allow-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    console.log("url: ", url)
+}).listen(3002)
